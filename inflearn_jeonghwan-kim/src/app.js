f27b1f9fb61c62bc0e8104c21572793d888390c8
Edwin <edwin@Edwinui-MacBookAir.local>
2023-12-16 07:59:39 +0900

    [추가] 요약 : 인프런 - 프론트엔드 개발환경 폴더 생성

---
+++ inflearn_jeonghwan-kim/src/app.js
@@ -0,0 +1,2 @@
+// console.log(sum(1,2))
+console.log(math.sum(1,2))
---
+++ inflearn_jeonghwan-kim/src/math.js
@@ -0,0 +1,11 @@
+// function sum (a,b) {
+//     return a + b
+// }
+
+var math = math || {};
+(function () {
+    function sum(a,b) {
+        return a+b;
+    }
+    math.sum = sum
+})()
